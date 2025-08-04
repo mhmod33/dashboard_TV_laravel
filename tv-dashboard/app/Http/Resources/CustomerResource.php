d027d6360c2577352e92be748f915f67d55e69f7
mhmod33 <me653830@gmail.com>
2025-08-04 18:16:40 +0300

    small edit on api retrieved data

--- tv-dashboard/app/Http/Resources/CustomerResource.php
+++ tv-dashboard/app/Http/Resources/CustomerResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,12 +16,14 @@ class CustomerResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'serial_number' => $this->serial_number,
             'name' => $this->customer_name,
             'address' => $this->address,
             'phone' => $this->phone,
             'payment_status' => $this->payment_status,
             'status' => $this->status,
+            'owner' => Admin::where('id', $this->admin_id)->first()->name,
             'created_at' => $this->created_at,
         ];
     }

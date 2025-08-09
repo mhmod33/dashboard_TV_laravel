91c0f67678135cb29a06d527f733708555a89ed3
MHMD <115650241+281mohamed@users.noreply.github.com>
2025-08-09 08:23:46 +0300

    m

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -16,7 +16,6 @@ class CustomerResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-<<<<<<< HEAD
         'id' => $this->id,
         'serial_number' => $this->serial_number,
         'name' => $this->customer_name,
@@ -29,17 +28,5 @@ class CustomerResource extends JsonResource
         'admin_id' => $this->admin_id,
         'created_at' => $this->created_at,
     ];
-=======
-            'id' => $this->id,
-            'serial_number' => $this->serial_number,
-            'name' => $this->customer_name,
-            'address' => $this->address,
-            'phone' => $this->phone,
-            'payment_status' => $this->payment_status,
-            'status' => $this->status,
-            'owner' => Admin::where('id', $this->admin_id)->first()->name,
-            'created_at' => $this->created_at,
-        ];
->>>>>>> 1a9a5ae6b0bcf07cab90eeff8f5d9f85cc33fbcb
     }
 }
